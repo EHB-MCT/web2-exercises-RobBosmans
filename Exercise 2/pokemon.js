72f64ea3f15baf8f98c10d5af5dd60cbb7f6c0fb
RobBosmans <rob.bosmans@student.ehb.be>
2021-10-05 11:34:40 +0200

    2

--- Exercise 2/pokemon.js
+++ Exercise 2/pokemon.js
@@ -1,16 +1,43 @@
-window.onload = function () {
-    console.log('loaded')
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
-        .then((response) => response.json())
-        .then((data) => {
-            const parent = document.getElementById('pokemon');
-            console.log(data.results[0]);
-            parent.innerHTML = `
-                <img src="" alt="pokemonImage">
-                <p>${data.results[0].name}</p>
-                <p>type</p>
-                <button>Add</button>
-            `;
-        });
+
+let list = [];
+let pokemon = [];
+
+function getData(){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then(response => {
+        return response.json();
+    }).then(data =>{
+            list = data.results;
             
+            //loop over list to get each pokemon
+            for(let element of list) {
+                fetch(element.url).then(response =>{
+                    return response.json();
+                }).then(data => {
+                    pokemon.push(data)
+                    console.log(pokemon);
+                })
+            };
+    })
 };
+
+window.onload = function(){
+    
+    getData();
+
+    setTimeout(buildList, 3000);
+    
+    
+    function buildList(){
+        let html = `
+        <img src="" alt="pokemonImage">
+        <p>${pokemon.name}</p>
+        <p>${pokemon.types}</p>
+        <button>Add</button>
+        `
+        document.getElementById('pokemon').innerHTML = html;
+    }
+    buildList();
+
+}
+
