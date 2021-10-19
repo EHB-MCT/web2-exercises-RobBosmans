7ee321885bab7a78fbe5f91acfc06d94e6477588
RobBosmans <rob.bosmans@student.ehb.be>
2021-10-19 12:52:01 +0200

    Update index.js

--- Exercise 4/src/index.js
+++ Exercise 4/src/index.js
@@ -1,9 +1,11 @@
 const Cleave = require("cleave.js");
 
-const cleave= require(Cleave)
+const cleave= require(Cleave);
 
-new Cleave('.fnumber', {
+new Cleave(".fnumber", {
     phone: true,
     phoneRegionCode: 'BE'
 });
+
+
 
