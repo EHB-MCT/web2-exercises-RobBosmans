7d8abfdc9e8d67c0846e7eb508cea0a7811d72ab
RobBosmans <56031137+RobBosmans@users.noreply.github.com>
2021-11-26 16:58:14 +0100

    display map and routes on map

--- Technical Test/script.js
+++ Technical Test/script.js
@@ -1,7 +1,6 @@
 // https://www.strava.com/oauth/token?client_id=41469&client_secret=0743f07f60f0eca5e78db28f88f78032a10067b8&code=306f962434700f1503a4478f7971af0f0f54e1f7&grant_type=authorization_code
 
 refreshToken = "c6d059e946455c72bfdaa2a4f78e8172c7a200a5";
-let accesToken = "";
 
 window.onload = function(){
     
@@ -12,7 +11,10 @@ async function getRoutes(data){
 
     //fetch routes
     let response = await fetch(`https://www.strava.com/api/v3/athletes/15412454/routes?access_token=${data.access_token}`);
-    return await console.log(response.json());
+    return await response.json()
+    .then(data => {
+        leafletMap(data);
+    });
 };
 
 function getNewAccessToken(){
@@ -36,5 +38,40 @@ function getNewAccessToken(){
     .catch(error => console.log('error', error));
 };
 
+function leafletMap(data){
 
+    //code from leafletjs.com
+    //creates map
+    let map = L.map('map').setView([50.7889886,4.168973,11], 11);
+
+    //uses mapbox
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoicm9iYm9zbWFucyIsImEiOiJja3dnampsMnkwcDluMm9wbWp6YW94M2dmIn0.YNnModWsj0A6qUNYPPUj9Q'
+    }).addTo(map);
+
+    //uses openstreetmap (backup)
+    /* L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map); */
+
+    for(let i=0; i < 5; i++){
+
+        let coordinates = L.Polyline.fromEncoded(data[i].map.summary_polyline).getLatLngs()
+        console.log(coordinates);
+
+        L.polyline(
+            coordinates,
+            {
+                color: "#FC5200",
+                weight: 2,
+                lineJoin: "round"
+            }
+        ).addTo(map);
+    }
+}
 
